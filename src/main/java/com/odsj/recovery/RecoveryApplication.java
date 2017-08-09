c9f068aa7fe2743165810a64e0589faead8aaff4
KIMJONGIL <nijin39@gmail.com>
2017-08-09 22:25:25 +0000

    project init

---
+++ src/main/java/com/odsj/recovery/RecoveryApplication.java
@@ -0,0 +1,12 @@
+package com.odsj.recovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoveryApplication.class, args);
+	}
+}
